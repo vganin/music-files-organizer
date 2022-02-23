cb8deda992121cec623b2adcf5be47f6477b81bd
Vsevolod Ganin <vsevolod.ganin@gmail.com>
2022-02-23 17:43:30 +0300

    Accumulate files and output path changes

--- src/main.rs
+++ src/main.rs
@@ -9,50 +9,73 @@ use id3::{Tag, TagLike};
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    #[clap(parse(from_os_str), value_name = "FILE")]
+    #[clap(parse(from_os_str), value_name = "INPUT FILE")]
+    input_file_path: PathBuf,
+
+    #[clap(parse(from_os_str), value_name = "OUTPUT FILE")]
+    output_file_path: PathBuf,
+}
+
+struct MusicFile {
     file_path: PathBuf,
+    tag: Tag,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let file_metadata = metadata(&args.file_path).unwrap();
+    if !metadata(&args.output_file_path).unwrap().is_dir() {
+        panic!("Output path is not directory")
+    }
 
-    if file_metadata.is_file() {
-        inspect_file(&args.file_path)
-    } else if file_metadata.is_dir() {
-        for entry in fs::read_dir(args.file_path).unwrap() {
-            let entry = entry.unwrap();
-            let path = entry.path();
-            inspect_file(&path);
+    let music_files = inspect_path(&args.input_file_path);
+
+    for music_file in music_files {
+        let tag = music_file.tag;
+        let mut path = PathBuf::from(&args.output_file_path);
+
+        path.push(tag.artist().unwrap());
+        path.push(format!("({}) {}", tag.year().unwrap(), tag.album().unwrap()));
+        match tag.disc() {
+            Some(disc) => path.push(format!("{}.{}. {}", disc, tag.track().unwrap(), tag.title().unwrap())),
+            None => path.push(format!("{}. {}", tag.track().unwrap(), tag.title().unwrap())),
         }
-    } else {
-        panic!("Given path is not a file nor a directory")
+
+        println!("Will write {}", path.display());
     }
 }
 
-fn inspect_file(path: impl AsRef<Path>) {
-    let tag = match Tag::read_from_path(path) {
-        Ok(tag) => tag,
+fn inspect_path(path: impl AsRef<Path>) -> Vec<MusicFile> {
+    let file_metadata = metadata(&path).unwrap();
+    if file_metadata.is_file() {
+        vec![inspect_file(&path)].into_iter().flatten().collect()
+    } else if file_metadata.is_dir() {
+        inspect_directory(&path)
+    } else {
+        vec![]
+    }
+}
+
+fn inspect_directory(path: impl AsRef<Path>) -> Vec<MusicFile> {
+    return fs::read_dir(path).unwrap()
+        .flat_map(|entry| {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            inspect_path(&path)
+        })
+        .collect();
+}
+
+fn inspect_file(path: impl AsRef<Path>) -> Option<MusicFile> {
+    println!("Inspecting file {}", path.as_ref().display());
+    match Tag::read_from_path(&path) {
+        Ok(tag) => Some(MusicFile {
+            file_path: PathBuf::from(path.as_ref()),
+            tag,
+        }),
         Err(error) => {
             println!("Can't read tags: {}", error);
-            return;
+            return None;
         }
-    };
-
-    // Get a bunch of frames...
-    if let Some(artist) = tag.artist() {
-        println!("artist: {}", artist);
-    }
-    if let Some(title) = tag.title() {
-        println!("title: {}", title);
-    }
-    if let Some(album) = tag.album() {
-        println!("album: {}", album);
-    }
-
-    // Get frames before getting their content for more complex tags.
-    if let Some(artist) = tag.get("TPE1").and_then(|frame| frame.content().text()) {
-        println!("artist: {}", artist);
     }
 }

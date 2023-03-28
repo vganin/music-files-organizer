1fac6b8d3ea8c9e97b2b454398bf94628e85883a
vsga <vsevolod.ganin@gmail.com>
2023-03-29 01:20:27 +0300

    Use lofty solo for duration picking

--- src/util/audio_file_duration.rs
+++ src/util/audio_file_duration.rs
@@ -4,13 +4,6 @@ use std::time::Duration;
 use anyhow::Result;
 use lofty::{AudioFile, Probe};
 
-use crate::util::path_extensions::PathExtensions;
-
 pub fn from_path(path: impl AsRef<Path>) -> Result<Option<Duration>> {
-    let extension = path.as_ref().extension_or_empty().to_lowercase();
-    match extension.as_ref() {
-        "mp3" => Ok(Some(mp3_duration::from_path(path)?)),
-        "flac" => Ok(Some(Probe::open(path)?.read()?.properties().duration())),
-        _ => Ok(None)
-    }
+    return Ok(Some(Probe::open(path)?.read()?.properties().duration()));
 }

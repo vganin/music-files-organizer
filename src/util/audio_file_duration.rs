31faeea7fa12051e06b0b2ed552d1b9bb906abb8
vsga <vsevolod.ganin@gmail.com>
2024-08-25 23:06:58 +0100

    Update dependencies

--- src/util/audio_file_duration.rs
+++ src/util/audio_file_duration.rs
@@ -2,7 +2,8 @@ use std::path::Path;
 use std::time::Duration;
 
 use anyhow::Result;
-use lofty::{AudioFile, Probe};
+use lofty::file::AudioFile;
+use lofty::probe::Probe;
 
 pub fn from_path(path: impl AsRef<Path>) -> Result<Option<Duration>> {
     return Ok(Some(Probe::open(path)?.read()?.properties().duration()));
